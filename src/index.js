759acca546187077d70dc0c7f5dce9e3198365c6
Edwin Montenegro <montenegro.edwin@gmail.com>
2022-04-28 21:18:13 -0500

    Fin del curso

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
-import ReactDOM from "react-dom/client";
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/index.js';
 import './index.css';
 
-// import { createRoot } from 'react-dom/client';
 
-// const container = document.getElementById('app');
-// const root = createRoot(container);
-// root.render(<App />);
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// ReactDOMClient.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const element = document.getElementById('root');
+const root = ReactDOM.createRoot(element);
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />);
-
-// const rootElement = document.getElementById('root');
-// const root = ReactDOMClient.createRoot(rootElement);
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+root.render(
+  <App />
+);
